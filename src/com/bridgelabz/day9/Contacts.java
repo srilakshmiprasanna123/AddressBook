bb2ee58104c55159eb0ea5dfc537a3f20f30f426
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-03-15 19:55:58 +0530

    Added UC2 Added a new contact

---
+++ src/com/bridgelabz/day9/Contacts.java
@@ -0,0 +1,96 @@
+package com.bridgelabz.day9;
+
+public class Contacts {
+
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private long zipCode;
+        private long phoneNumber;
+        private String email;
+
+        public Contacts() {
+
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public long getZipCode() {
+            return zipCode;
+        }
+
+        public void setZipCode(long zipCode) {
+            this.zipCode = zipCode;
+        }
+
+        public long getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(long phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        @Override
+        public String toString() {
+            return "Contacts{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", address='" + address + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    ", zipCode=" + zipCode +
+                    ", phoneNumber=" + phoneNumber +
+                    ", email='" + email + '\'' +
+                    '}';
+        }
+    }
+
