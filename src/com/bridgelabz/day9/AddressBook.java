3985cfb9a3e17cd553b29af7de01dc486b27b5bf
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-03-15 20:08:49 +0530

    Added UC3 Added edit contact

--- src/com/bridgelabz/day9/AddressBook.java
+++ src/com/bridgelabz/day9/AddressBook.java
@@ -34,8 +34,95 @@ public class AddressBook {
         System.out.println(Book);
     }
 
+
+    public void editDetails() {
+
+        System.out.println("Confirm your first name to edit details: ");
+        String name = scanner.next();
+
+        for (int i = 0; i < Book.size(); i++) {
+            if (Book.get(i).getFirstName().equals(name)) {
+                System.out.println("Select form below to change: ");
+                System.out.println(
+                        "\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Email\n7.Zip\n8.Phone number");
+                int check = scanner.nextInt();
+
+                switch (check) {
+                    case 1:
+                        System.out.println("Enter first name");
+                        Book.get(i).setFirstName(scanner.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter Last name");
+                        Book.get(i).setLastName(scanner.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter address");
+                        Book.get(i).setAddress(scanner.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter city");
+                        Book.get(i).setCity(scanner.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter state");
+                        Book.get(i).setState(scanner.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter email");
+                        Book.get(i).setEmail(scanner.next());
+                        break;
+                    case 7:
+                        System.out.println("Enter Zip");
+                        Book.get(i).setZipCode(scanner.nextInt());
+                        break;
+                    case 8:
+                        System.out.println("Enter phone number");
+                        Book.get(i).setPhoneNumber(scanner.nextInt());
+                        break;
+                }
+
+                System.out.println(Book);
+
+            } else
+                System.out.println("Enter valid First name");
+        }
+
+    }
+
+    /*
+     * Purpose : Used output method to display the details
+     */
+    public void output() {
+        // TODO Auto-generated method stub
+        System.out.println(Book);
+    }
+
     public static void main(String[] args) {
+        int i = 0;
+
         System.out.println("Welcome to Address Book Management System");
+
         AddressBook details = new AddressBook();
+
+        while (i == 0) {
+            System.out.println("What you want to do: ");
+            System.out.println("1.Add details.\n2.Edit details.");
+            int choose = scanner.nextInt();
+            switch (choose) {
+                case 1:
+                    details.AddDetails();
+                    break;
+                case 2:
+                    details.editDetails();
+                    break;
+                default:
+                    i = 1;
+                    System.out.println("Wrong option");
+                    details.output();
+                    break;
+            }
+        }
+
     }
 }
